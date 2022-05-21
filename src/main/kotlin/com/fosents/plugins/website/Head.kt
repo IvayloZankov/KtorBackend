cd7be54b686d26fdc40a6487906217800a484077
Ivailo Zankov <fosents@gmail.com>
2022-05-22 01:16:10 +0300

    og properties

--- src/main/kotlin/com/fosents/plugins/website/Head.kt
+++ src/main/kotlin/com/fosents/plugins/website/Head.kt
@@ -12,9 +12,27 @@ fun HTML.setHead() {
             name = "color-scheme"
             content = "light only"
         }
-        link(rel = "stylesheet", href = "/styles.css", type = "text/css")
-        title {
-            +"zankov.dev"
+        meta {
+            attributes["property"] = "og:url"
+            content = "https://zankov.dev/"
         }
+        meta {
+            attributes["property"] = "og:image"
+            content = "https://zankov.dev/ktor.png"
+        }
+        meta {
+            attributes["property"] = "og:title"
+            content = "Ivaylo Zankov - My ktor website"
+        }
+        meta {
+            attributes["property"] = "og:description"
+            content = "My latest projects I'm developing and some info about me."
+        }
+        link(rel = "stylesheet", href = "/styles.css", type = "text/css")
+        link(rel = "icon", href = "/robot.svg")
+        title {
+            +"Ivaylo Zankov"
+        }
+
     }
 }
